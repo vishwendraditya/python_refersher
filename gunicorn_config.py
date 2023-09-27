028bd81f93742f075d38b9804e5c181f02a52d64
VISHWENDRADITYA KUMAWAT <vishwendraditya@gmail.com>
2023-09-27 14:15:40 +0530

    Added my project

---
+++ gunicorn_config.py
@@ -0,0 +1,2 @@
+workers = 4  # Adjust the number of workers based on your server's resources
+bind = '0.0.0.0:8000'  # Use the appropriate IP and port
---
+++ main.py
@@ -0,0 +1,5 @@
+# main.py
+import app  # Import your Flask application from the compiled .pyc file
+
+if __name__ == '__main__':
+    app.app.run()
